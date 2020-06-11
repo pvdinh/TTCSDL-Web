2c5ac8b9bf51e8b9e598fdd272f0a16526e17101
bodinh <61085282+bodinh@users.noreply.github.com>
2020-06-11 15:17:20 +0700

    Revert "Hungcute"

--- CAFE_Song Lo/Models/classdata.cs
+++ CAFE_Song Lo/Models/classdata.cs
@@ -1,5 +1,4 @@
-﻿using PagedList;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,7 +12,7 @@ namespace CAFE_Song_Lo.Models
         public List<food> allfoods { get; set; }
         public List<bill> allbills { get; set; }
         public List<billinfo> allbillinfos { get; set; }
-        
+
         public List<int?> top10sell { get; set; }
 
         public double? Tongtien { get; set; }
